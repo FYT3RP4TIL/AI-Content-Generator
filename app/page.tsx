c5c3301409354fb09679e271ca9e6ba25229895b
FYT3RP4TIL <sudhanshu21032002@gmail.com>
2024-08-18 01:03:43 +0530

    Created Base App

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { ButtonIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Sudhanshu</h2>
+      <Button>Pay</Button>
+    </div>
+  );
+}
