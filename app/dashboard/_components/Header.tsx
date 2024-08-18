602e10d252144666773ffb6762ad65f255e5f162
FYT3RP4TIL <sudhanshu21032002@gmail.com>
2024-08-19 00:23:00 +0530

    Final Dashboard Component Changes

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,21 +1,24 @@
-import { Search } from "lucide-react";
-import React from "react";
+import { UserButton } from '@clerk/nextjs'
+import { Search } from 'lucide-react'
+import React from 'react'
 
 function Header() {
   return (
-    <div className="p-5 shadow-sm border-b-2 bg-white flex justify-between items-center">
-      <div className="flex gap-2 items-center p-2 border rounded-md max-w-lg bg-white">
-        <Search />
-        <input type="text" placeholder="Search..." className="outline-none" />
+    <div className='p-5 shadow-sm border-b-2 bg-white flex justify-between items-center'>
+      <div className='flex gap-2 items-center
+       p-2 border rounded-md max-w-lg bg-white'>
+        <Search/>
+        <input type='text' placeholder='Search...'
+        className='outline-none'
+        />
       </div>
-      <hr className="my-6 border" />
-      <div className="mt-3">
-        <h2 className="bg-primary p-1 rounded-full text-xs text-white px-2">
-          🔥 Join Membership just for $9.99/month
-        </h2>
+      <div className='flex gap-5 items-center'>
+        <h2 className='bg-primary p-1 rounded-full text-sm text-white px-2'>
+        🔥 Join Membership just for Rs.999/Month</h2>
+      <UserButton/>
       </div>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
